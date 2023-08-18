acd350fdf8cebc87e7e35552cb4294febeec1b96
al@mastercode.io <al@mastercode.io>
2023-08-18 13:50:30 +0000

    Edited settings and dev

--- client_code/dev.py
+++ client_code/dev.py
@@ -3,9 +3,11 @@ from anvil.js.window import jQuery, ej
 from AnvilFusion.tools.utils import AppEnv, init_user_session
 from AnvilFusion.datamodel import migrate
 from . import app
+from anvil.tables import app_tables
 
 AppEnv.APP_ID = 'practiceMANAGER'
 AppEnv.data_models = app.models
 
 init_user_session()
-migrate.migrate_db_schema()
+# migrate.migrate_db_schema()
+
