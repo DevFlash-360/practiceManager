6aaf6120c5fe5f1d04a3309a8c5bf4a2a25f4f46
Alex Sherin <al@mastercode.io>
2023-08-28 10:59:06 +0100

    Alex remote commit

--- client_code/Forms/CaseWorkflowForm.py
+++ client_code/Forms/CaseWorkflowForm.py
@@ -17,7 +17,7 @@ class CaseWorkflowForm(FormBase):
                 {'name': 'type.name', 'label': 'Type', 'width': '10%'},
                 {'name': 'activity.name', 'label': 'Activity', 'width': '25%'},
                 {'name': 'notes', 'label': 'Notes', 'width': '40%'},
-                {'name': 'assigned_to.full_name', 'label': 'Assigned To', 'width': '25%'},
+                # {'name': 'assigned_to.full_name', 'label': 'Assigned To', 'width': '25%'},
             ],
         }
         self.items = SubformGrid(name='items', label='Items', model='CaseWorkflowItem',
