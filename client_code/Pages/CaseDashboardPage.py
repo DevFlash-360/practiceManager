3c6c7f3640864e38a0d7a75d407b95a081886b73
Alex Sherin <al@mastercode.io>
2023-09-07 15:12:03 +0100

    Alex remote commit

--- client_code/Pages/CaseDashboardPage.py
+++ client_code/Pages/CaseDashboardPage.py
@@ -8,6 +8,30 @@ class CaseDashboardPage(DashboardPage):
         layout = {
             'cellSpacing': [10, 10],
             'columns': 3,
+            'panels': [
+                {'sizeX': 1, 'sizeY': 2, 'row': 0, 'col': 0, 'id': 'case_details',
+                 'content': '<div class="content" style="line-height:60px">Case</div>'},
+                {'sizeX': 2, 'sizeY': 1, 'row': 1, 'col': 0, 'id': 'incident_date',
+                 'content': '<div class="content" style="line-height:60px">Incident Date</div>'},
+                {'sizeX': 1, 'sizeY': 1, 'row': 3, 'col': 0, 'id': 'contacts',
+                 'content': '<div class="content" style="line-height:60px">Contacts</div>'},
+                {'sizeX': 1, 'sizeY': 1, 'row': 1, 'col': 1, 'id': 'cuase_of_action',
+                 'content': '<div class="content" style="line-height:60px">Cause(s) of Action</div>'},
+                {'sizeX': 1, 'sizeY': 1, 'row': 1, 'col': 1, 'id': 'custody_status',
+                 'content': '<div class="content" style="line-height:60px">Custody Status</div>'},
+                {'sizeX': 1, 'sizeY': 1, 'row': 0, 'col': 2, 'id': 'assigned_attorney',
+                 'content': '<div class="content" style="line-height:60px">Assigned Attorney(s)</div>'},
+                {'sizeX': 1, 'sizeY': 1, 'row': 1, 'col': 2, 'id': 'case_status',
+                 'content': '<div class="content" style="line-height:60px">Case Status</div>'},
+                {'sizeX': 1, 'sizeY': 2, 'row': 2, 'col': 2, 'id': 'case_payments',
+                 'content': '<div class="content" style="line-height:60px">Payment Status</div>'},
+                {'sizeX': 1, 'sizeY': 2, 'row': 4, 'col': 2, 'id': 'time_entries',
+                 'content': '<div class="content" style="line-height:60px">Time Entries</div>'},
+                {'sizeX': 1, 'sizeY': 1, 'row': 6, 'col': 2, 'id': 'case_expenses',
+                 'content': '<div class="content" style="line-height:60px">Expenses</div>'},
+                {'sizeX': 1, 'sizeY': 1, 'row': 7, 'col': 2, 'id': 'case_balances',
+                 'content': '<div class="content" style="line-height:60px">Balances</div>'},
+            ],
         }
         
         super().__init__(container_id=container_id, layout=layout, **kwargs)
