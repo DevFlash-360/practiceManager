f8d06484da81a2f7d984db1e9b3daa7a183ff38b
Alex Sherin <al@mastercode.io>
2023-09-11 15:39:54 +0100

    Alex remote commit

--- client_code/Pages/CaseDashboardPage.py
+++ client_code/Pages/CaseDashboardPage.py
@@ -6,7 +6,6 @@ class CaseDashboardPage(DashboardPage):
     def __init__(self, container_id, **kwargs):
         
         layout = {
-            'showGridLines': True,
             'cellSpacing': [10, 10],
             'columns': 3,
             'cellAspectRatio': 100/50,
@@ -18,17 +17,17 @@ class CaseDashboardPage(DashboardPage):
                 {
                     'sizeX': 1, 'sizeY': 2, 'row': 1, 'col': 0,
                     'id': 'incident_date', 'header': 'Incident Date'
-                 },
-                {
-                    'sizeX': 1, 'sizeY': 1, 'row': 3, 'col': 0,
-                    'id': 'contacts',   'header': 'Contacts',
                 },
                 {
-                    'sizeX': 1, 'sizeY': 1, 'row': 1, 'col': 1,
+                    'sizeX': 1, 'sizeY': 1, 'row': 3, 'col': 0,
                     'id': 'cause_of_action', 'header': 'Cause(s) of Action',
                 },
                 {
                     'sizeX': 1, 'sizeY': 1, 'row': 1, 'col': 1,
+                    'id': 'case_status', 'header': 'Case Status',
+                },
+                {
+                    'sizeX': 1, 'sizeY': 1, 'row': 2, 'col': 1,
                     'id': 'custody_status', 'header': 'Custody Status',
                 },
                 {
@@ -36,24 +35,24 @@ class CaseDashboardPage(DashboardPage):
                     'id': 'assigned_attorney', 'header': 'Assigned Attorney(s)',
                 },
                 {
-                    'sizeX': 1, 'sizeY': 1, 'row': 1, 'col': 2,
-                    'id': 'case_status', 'header': 'Case Status',
-                 },
+                    'sizeX': 1, 'sizeY': 2, 'row': 1, 'col': 2,
+                    'id': 'contacts',   'header': 'Contacts',
+                },
                 {
-                    'sizeX': 1, 'sizeY': 2, 'row': 2, 'col': 2,
+                    'sizeX': 1, 'sizeY': 1, 'row': 4, 'col': 0,
                     'id': 'case_payments', 'header': 'Payment Status',
-                 },
+                },
                 {
-                    'sizeX': 1, 'sizeY': 2, 'row': 4, 'col': 2,
+                    'sizeX': 1, 'sizeY': 1, 'row': 5, 'col': 0,
+                    'id': 'case_balances', 'header': 'Balances',
+                },
+                {
+                    'sizeX': 2, 'sizeY': 1, 'row': 4, 'col': 1,
                     'id': 'time_entries', 'header': 'Time Entries',
                 },
                 {
-                    'sizeX': 1, 'sizeY': 1, 'row': 6, 'col': 2,
+                    'sizeX': 2, 'sizeY': 1, 'row': 5, 'col': 1,
                     'id': 'case_expenses', 'header': 'Expenses',
-                },
-                {
-                    'sizeX': 1, 'sizeY': 1, 'row': 7, 'col': 2,
-                    'id': 'case_balances', 'header': 'Balances',
                 },
             ],
         }
