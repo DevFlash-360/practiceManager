aa91ac18e63e69482dc85a9b063fb1fa6e72dfdf
Alex Sherin <al@mastercode.io>
2023-09-11 15:32:06 +0100

    Alex remote commit

--- client_code/Pages/CaseDashboardPage.py
+++ client_code/Pages/CaseDashboardPage.py
@@ -11,28 +11,50 @@ class CaseDashboardPage(DashboardPage):
             'columns': 3,
             'cellAspectRatio': 100/50,
             'panels': [
-                {'sizeX': 2, 'sizeY': 1, 'row': 0, 'col': 0, 'id': 'case_details',
-                 'content': '<div class="content" style="line-height:60px">Case</div>'},
-                {'sizeX': 1, 'sizeY': 2, 'row': 1, 'col': 0, 'id': 'incident_date',
-                 'content': '<div class="content" style="line-height:60px">Incident Date</div>'},
-                {'sizeX': 1, 'sizeY': 1, 'row': 3, 'col': 0, 'id': 'contacts',
-                 'content': '<div class="content" style="line-height:60px">Contacts</div>'},
-                {'sizeX': 1, 'sizeY': 1, 'row': 1, 'col': 1, 'id': 'cuase_of_action',
-                 'content': '<div class="content" style="line-height:60px">Cause(s) of Action</div>'},
-                {'sizeX': 1, 'sizeY': 1, 'row': 1, 'col': 1, 'id': 'custody_status',
-                 'content': '<div class="content" style="line-height:60px">Custody Status</div>'},
-                {'sizeX': 1, 'sizeY': 1, 'row': 0, 'col': 2, 'id': 'assigned_attorney',
-                 'content': '<div class="content" style="line-height:60px">Assigned Attorney(s)</div>'},
-                {'sizeX': 1, 'sizeY': 1, 'row': 1, 'col': 2, 'id': 'case_status',
-                 'content': '<div class="content" style="line-height:60px">Case Status</div>'},
-                {'sizeX': 1, 'sizeY': 2, 'row': 2, 'col': 2, 'id': 'case_payments',
-                 'content': '<div class="content" style="line-height:60px">Payment Status</div>'},
-                {'sizeX': 1, 'sizeY': 2, 'row': 4, 'col': 2, 'id': 'time_entries',
-                 'content': '<div class="content" style="line-height:60px">Time Entries</div>'},
-                {'sizeX': 1, 'sizeY': 1, 'row': 6, 'col': 2, 'id': 'case_expenses',
-                 'content': '<div class="content" style="line-height:60px">Expenses</div>'},
-                {'sizeX': 1, 'sizeY': 1, 'row': 7, 'col': 2, 'id': 'case_balances',
-                 'content': '<div class="content" style="line-height:60px">Balances</div>'},
+                {
+                    'sizeX': 2, 'sizeY': 1, 'row': 0, 'col': 0,
+                    'id': 'case_details', 'header': 'Case Details',
+                },
+                {
+                    'sizeX': 1, 'sizeY': 2, 'row': 1, 'col': 0,
+                    'id': 'incident_date', 'header': 'Incident Date'
+                 },
+                {
+                    'sizeX': 1, 'sizeY': 1, 'row': 3, 'col': 0,
+                    'id': 'contacts',   'header': 'Contacts',
+                },
+                {
+                    'sizeX': 1, 'sizeY': 1, 'row': 1, 'col': 1,
+                    'id': 'cause_of_action', 'header': 'Cause(s) of Action',
+                },
+                {
+                    'sizeX': 1, 'sizeY': 1, 'row': 1, 'col': 1,
+                    'id': 'custody_status', 'header': 'Custody Status',
+                },
+                {
+                    'sizeX': 1, 'sizeY': 1, 'row': 0, 'col': 2,
+                    'id': 'assigned_attorney', 'header': 'Assigned Attorney(s)',
+                },
+                {
+                    'sizeX': 1, 'sizeY': 1, 'row': 1, 'col': 2,
+                    'id': 'case_status', 'header': 'Case Status',
+                 },
+                {
+                    'sizeX': 1, 'sizeY': 2, 'row': 2, 'col': 2,
+                    'id': 'case_payments', 'header': 'Payment Status',
+                 },
+                {
+                    'sizeX': 1, 'sizeY': 2, 'row': 4, 'col': 2,
+                    'id': 'time_entries', 'header': 'Time Entries',
+                },
+                {
+                    'sizeX': 1, 'sizeY': 1, 'row': 6, 'col': 2,
+                    'id': 'case_expenses', 'header': 'Expenses',
+                },
+                {
+                    'sizeX': 1, 'sizeY': 1, 'row': 7, 'col': 2,
+                    'id': 'case_balances', 'header': 'Balances',
+                },
             ],
         }
         
