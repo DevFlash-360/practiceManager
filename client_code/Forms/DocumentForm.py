75e3a1a695e787b096b6cb1784dacc5b26d236da
Alex Sherin <al@mastercode.io>
2023-09-15 23:39:13 +0100

    Alex remote commit

--- client_code/Forms/DocumentForm.py
+++ client_code/Forms/DocumentForm.py
@@ -16,9 +16,11 @@ class DocumentForm(FormBase):
     def __init__(self, **kwargs):
         print('DocumentForm')
         kwargs['model'] = 'Document'
-        self.case = LookupInput(name='case', label='Case', model='Case', text_field='case_name')
+        self.case = LookupInput(name='case', label='Case', model='Case', text_field='case_name',
+                                on_change=self.case_selected)
         self.folder = LookupInput(name='folder', label='Folder', model='DocumentFolder', text_field='name',
-                                  add_item_label='Create Folder', add_item_form=FormBase)
+                                  add_item_label='Create Folder', add_item_form=FormBase,
+                                  on_change=self.folder_selected)
         self.type = DropdownInput(name='type', label='Document Type', options=DOCUMENT_TYPES)
         self.discovery = CheckboxInput(name='discovery', label='Mark as Discovery')
         self.reviewed_by = LookupInput(name='reviewed_by', label='Reviewed By', model='Staff', text_field='full_name')
@@ -48,11 +50,34 @@ class DocumentForm(FormBase):
 
     def form_open(self, args):
         super().form_open(args)
+        # if self.case.value is None:
+        #     self.folder.enabled = False
+        # if not self.folder.enabled:
+        #     self.type.enabled = False
+        #     self.discovery.enabled = False
+        #     self.reviewed_by.enabled = False
+        #     self.notes.enabled = False
+        #     self.file.enabled = False
+
+
+    def case_selected(self, args):
         if self.case.value is None:
             self.folder.enabled = False
-        if not self.folder.enabled:
+        else:
+            self.folder.enabled = True
+            self.folder_selected({})
+
+
+    def folder_selected(self, args):
+        if self.folder.value is None:
             self.type.enabled = False
             self.discovery.enabled = False
             self.reviewed_by.enabled = False
             self.notes.enabled = False
             self.file.enabled = False
+        else:
+            self.type.enabled = True
+            self.discovery.enabled = True
+            self.reviewed_by.enabled = True
+            self.notes.enabled = True
+            self.file.enabled = True
