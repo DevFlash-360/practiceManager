cd918332ee2646ba115d16d398fea9514db2af8e
Alex Sherin <al@mastercode.io>
2023-09-18 13:19:04 +0100

    Alex remote commit

--- client_code/Forms/DocumentForm.py
+++ client_code/Forms/DocumentForm.py
@@ -25,7 +25,7 @@ class DocumentForm(FormBase):
         self.discovery = CheckboxInput(name='discovery', label='Mark as Discovery')
         self.reviewed_by = LookupInput(name='reviewed_by', label='Reviewed By', model='Staff', text_field='full_name')
         self.notes = MultiLineInput(name='notes', label='Notes', rows=7)
-        self.file = FileUploadInput(name='file', label='Upload File', on_change=self.file_selected)
+        self.file = FileUploadInput(name='file', label='Upload File')
 
         sections = [
             {'name': '_', 'cols': [
