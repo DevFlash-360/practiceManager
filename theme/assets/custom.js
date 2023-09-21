cc4fda64541f09160680041c755c6ec64c0b0906
adam@theplumegroup.com <adam@theplumegroup.com>
2023-09-21 17:38:19 +0000

    Edited custom.js

--- theme/assets/custom.js
+++ theme/assets/custom.js
@@ -6,3 +6,12 @@ $(document).on('click','#pm-appbar-sidebar-toggle',function() {
     $("#pm-content").css("margin-left", "280px");
   }
 });
+$(document).on('click','.e-list-item',function() {
+  console.log('testing');
+  if ($("#pm-sidebar").hasClass("e-close")) {
+    $("#pm-content").removeClass("pm-custom-margin");
+  } 
+  if ($("#pm-sidebar").hasClass("e-open")) {
+    $("#pm-content").addClass("pm-custom-margin");
+  }
+});
