f3958b6619d6e31b0280bc69eaf951d3659e9f39
Alex Sherin <al@mastercode.io>
2023-09-23 21:52:08 +0100

    Alex remote commit

--- client_code/Views/CaseDocumentsView.py
+++ client_code/Views/CaseDocumentsView.py
@@ -53,7 +53,7 @@ class CaseDocumentsView(GridView):
 
     def collapse_all(self, args):
         if self.first_load:
-            self.grid.groupSettings.captionTemplate = '<div>${console.log("Caption: " + data)}</div>'
+            self.grid.groupSettings.captionTemplate = '<div>${key}</div>'
             self.grid.groupModule.collapseAll()
             self.first_load = False
 
