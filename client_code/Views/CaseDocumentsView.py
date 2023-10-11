e1d689c5ecac3d8ef8d5f81c6723865b682d1672
Alex Sherin <al@mastercode.io>
2023-10-11 16:15:50 +0100

    Alex remote commit

--- client_code/Views/CaseDocumentsView.py
+++ client_code/Views/CaseDocumentsView.py
@@ -40,7 +40,7 @@ class CaseDocumentsView(GridView):
         #     'allowDeleting': True,
         #     'mode': 'Normal',
         # }
-        # self.grid.dataBound = self.collapse_all
+        self.grid.dataBound = self.collapse_all
         self.first_load = True
 
 
