b718997061c2eb1ab99e46ce8d2bec534385647c
Alex Sherin <al@mastercode.io>
2023-11-02 22:12:49 +0000

    Alex remote commit

--- client_code/Pages/CaseDashboardPage.py
+++ client_code/Pages/CaseDashboardPage.py
@@ -115,4 +115,55 @@ class CaseDashboardPage(DashboardPage):
                 'id': 'cause_of_action',
                 'content': panel_content,
             })
-    
+            # case status
+            panel_content = f"<h5>{self.case['case_status']['name']}</h5>"
+            panel_content += f"<h6>Last Update</h6>TBD"
+            panel_content = f"<div style='width:100%;height:100%;overflow:auto;'>{panel_content}</div>"
+            self.dashboard.updatePanel({
+                'id': 'case_status',
+                'content': panel_content,
+            })
+            # custody status
+            panel_content = f"<h6>Custody Status</h6>TBD"
+            panel_content += f"<h6>Jail/Prison</h6>TBD"
+            panel_content += f"<h6>Inmate ID</h6>TBD"
+            panel_content += f"<h6>Bail Status</h6>TBD"
+            panel_content = f"<div style='width:100%;height:100%;overflow:auto;'>{panel_content}</div>"
+            self.dashboard.updatePanel({
+                'id': 'custody_status',
+                'content': panel_content,
+            })
+            # assigned attorney
+            panel_content = f"<ul>"
+            for attorney in self.case['assigned_attorneys']:
+                panel_content += f"<li>{attorney['full_name']}</li>"
+            panel_content += f"</ul>"
+            panel_content = f"<div style='width:100%;height:100%;overflow:auto;'>{panel_content}</div>"
+            self.dashboard.updatePanel({
+                'id': 'assigned_attorney',
+                'content': panel_content,
+            })
+            # contacts
+            panel_content = f"<ul>"
+            for contact in self.case['contacts']:
+                panel_content += f"<li>{contact['full_name']} - {contact['contact_group']['name']}</li>"
+            panel_content += f"</ul>"
+            panel_content = f"<div style='width:100%;height:100%;overflow:auto;'>{panel_content}</div>"
+            self.dashboard.updatePanel({
+                'id': 'contacts',
+                'content': panel_content,
+            })
+            # case payments
+            panel_content = f"<h5>TBD</h5>"
+            panel_content += f"<h6>Fee Type</h6>{self.case['fee_type']['name']}"
+            panel_content += f"<h6>Retainer</h6>${(self.case['flat_fee_retainer'] or self.case['hourly_retainer']):,.2f}"
+            panel_content += f"<h6>Trial</h6>" + "Not " if not self.case['trial_included'] else "" + "Included"
+            panel_content += f"<h6>Retainer Hour Limit</h6>${self.case['retainer_hours_limit'] or 'Unlimited'}"
+            panel_content += f"<h6>Investigator</h6>" + "Not " if not self.case['investigator'] else "" + "Included"
+            panel_content += f"<h6>Investigator Budget</h6>${self.case['investigator_budget']:,.2f}"
+            panel_content += f"<h6>Seal/Expungement</h6>" + "Not " if not self.case['record_seal_expungement'] else "" + "Included"
+            panel_content = f"<div style='width:100%;height:100%;overflow:auto;'>{panel_content}</div>"
+            self.dashboard.updatePanel({
+                'id': 'case_payments',
+                'content': panel_content,
+            })
