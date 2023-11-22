378efaf5e1b90a5a914b6e92f16c8a239634b07e
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2023-11-22 16:48:20 -0500

    dmytro commit

--- client_code/Views/TaskListView.py
+++ client_code/Views/TaskListView.py
@@ -118,8 +118,7 @@ class TaskListView(GridView2):
             self.grid.clearFiltering()
 
     def handler_filter_staff(self, args):
-        print(self.grid)
-        self.grid.filterByColumn('assigned_staff', 'contains', args['itemData']['Text'])
+        self.grid.filterByColumn('assigned_staff.full_name', 'contains', args['itemData']['Text'])
 
     def grid_action_handler(self, args):
         super().grid_action_handler(args)
