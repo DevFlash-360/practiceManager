d9fd7fff44c9b5eb5693f88a3d434bbe56508dec
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2023-11-28 11:18:42 -0500

    dmytro commit

--- client_code/Views/TaskListView.py
+++ client_code/Views/TaskListView.py
@@ -182,7 +182,7 @@ class TaskListView(GridView2):
 		# 													search_queries=self.search_queries,
 		# 													filters=self.filters,
 		# 													include_rows=False)
-        self.update_grid(args, False)
+        self.update_grid(args['rowData'], False)
         # self.invalidate()
         # self.grid.refresh()
         # print(F"completed = {obj['completed']}, notes = {obj['notes']}")
