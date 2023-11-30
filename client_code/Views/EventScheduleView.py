f8cb0198bfecd910fa678c2b032e23e534f7ab83
dmytrosav <dmytro.savchenko.solutions@gmail.com>
2023-11-30 20:39:12 +0200

    dmytro fix task filter

--- client_code/Views/EventScheduleView.py
+++ client_code/Views/EventScheduleView.py
@@ -236,7 +236,7 @@ class EventScheduleView:
 
     def get_tasks(self, start_time, end_time):
         query = {
-            'due_date': q.all_of(q.greater_than(start_time), q.less_than(end_time)),
+            'due_date': q.all_of(q.greater_than_or_equal_to(start_time), q.less_than_or_equal_to(end_time)),
             'completed': False
         }
         event_cols = [
