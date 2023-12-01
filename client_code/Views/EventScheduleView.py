4a45066ba21768d8cd6f99951b245a4a1ef517c1
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2023-11-30 22:02:40 -0500

    dmytro

--- client_code/Views/EventScheduleView.py
+++ client_code/Views/EventScheduleView.py
@@ -250,6 +250,7 @@ class EventScheduleView:
 
         self.tasks = []
         tasks = Task.get_grid_view(view_config={'columns':event_cols}, filters=query)
+        print(tasks)
         for task in tasks:
             item = {}
             item['event_type'] = PM_SCHEDULE_TYPE_TASK
