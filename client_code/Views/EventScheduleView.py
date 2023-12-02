70d646c48ae65c3ce2167aece504bad70f9fcae5
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2023-12-02 15:25:29 -0500

    dmytro

--- client_code/Views/EventScheduleView.py
+++ client_code/Views/EventScheduleView.py
@@ -106,9 +106,10 @@ class EventScheduleView:
         self.init_filters()
 
     def init_filters(self):
-        cases_data = Case.search()
+        # cases_data = Case.search()
         print(f"========= 1 ===========")
         
+        cases_data = [{'text': case['case_name']} for case in Case.search()]
         cases_data_for_dropdown = [{'text': case['case_name']} for case in cases_data]
         print("========= 2 ===========")
         cases_data_for_dropdown.insert(0, {'Id': 'all', 'Text': 'All cases'})
