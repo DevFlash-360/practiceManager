0031a9b9ffdad4f35624126328248920b2266ddd
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2023-12-08 16:28:08 -0500

    dmytro

--- client_code/Views/EventScheduleView.py
+++ client_code/Views/EventScheduleView.py
@@ -245,7 +245,7 @@ class EventScheduleView:
 
     def get_events(self, start_time, end_time):
         query = {'start_time': q.all_of(q.greater_than(start_time), q.less_than(end_time))}
-        cases_test1 = Case.get('18cdfe25-cbbf-4aa8-b8ce-b8ecc60b4ea9')
+        cases_test1 = Case.get('5dcd1159-d743-449e-9ace-2917f0267313')
         cases_test2 = Case.get('6f9d60d8-fe11-4909-85fe-bafe16e4e26e')
         if self.cases_filters:
             query['case'] = q.any_of(cases_test1, cases_test2)
