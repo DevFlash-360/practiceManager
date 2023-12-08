0fc3195d399053ede3b73e4cc2a5bcd5e0c42129
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2023-12-08 18:17:22 -0500

    dmytro

--- client_code/Views/EventScheduleView.py
+++ client_code/Views/EventScheduleView.py
@@ -244,8 +244,6 @@ class EventScheduleView:
             self.schedule.openQuickInfoPopup(event)
 
     def get_events(self, start_time, end_time):
-        cases_test1 = Case.get('18cdfe25-cbbf-4aa8-b8ce-b8ecc60b4ea9')
-        cases_test2 = Case.get('6f9d60d8-fe11-4909-85fe-bafe16e4e26e')
         query = {
             'start_time': q.all_of(q.greater_than(start_time), q.less_than(end_time)),
             'case.case_name': q.any_of('Playwright Case', 'Playwright Updated Case')
