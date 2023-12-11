6c5aacce40ad8d8e4af47fa2f50d8115d1ff1023
dmytro.savchenko.solutions@gmail.com <dmytro.savchenko.solutions@gmail.com>
2023-12-11 18:44:52 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -29,4 +29,8 @@ def get_cases(ids):
 
 @anvil.server.callable
 def get_events(case_ids):
-  
+  if case_ids:
+      cases = [case for case in app_tables.cases.search(uid=q.any_of(case_ids))]
+  else:
+      cases = [case for case in app_tables.cases.search()]
+  return cases
