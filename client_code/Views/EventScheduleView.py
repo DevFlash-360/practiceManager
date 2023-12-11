bf1940ef928771f6c16548037af046246eb19c0c
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2023-12-11 15:59:26 -0500

    dmytro

--- client_code/Views/EventScheduleView.py
+++ client_code/Views/EventScheduleView.py
@@ -283,7 +283,11 @@ class EventScheduleView:
             item['end_time_time'] = event['end_time'].strftime('%H:%M')
             item['staff_name'] = ' '.join([f"{staff['first_name']} {staff['last_name']}" for staff in event['staff']])
             item['location_name'] = event['location']['name'] if event['location']['name'] else ''
-            item['department'] = f"{event['department']['first_name']} {event['department']['last_name']} - {event['department']['title_position']}" if event['department']['full_name'] else ''
+            item['department'] = ""
+            if event['department']['first_name'] and event['department']['last_name']:
+                item['department'] = f"{item['department']['first_name']} {event['department']['last_name']}" 
+            if event['department']['title_position']:
+                item['department'] = f"{item['department']} {item['department']['title_position']}"
             self.events.append(item)
         self.schedules = self.events + self.tasks
 
