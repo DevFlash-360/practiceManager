2c86382f06facd3fd5314329344b59bd94d63380
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2023-12-15 14:53:28 -0500

    dmytro

--- client_code/Views/EventScheduleView.py
+++ client_code/Views/EventScheduleView.py
@@ -119,6 +119,7 @@ class EventScheduleView:
             'views': PM_SCHEDULE_DEFAULT_VIEWS,
             'selectedDate': Date.now(),
             'disableHtmlEncode': False, 
+            'showHeaderBar': True,
             'eventSettings': {
                 'dataSource': self.data_manager,
                 'fields': event_fields,
