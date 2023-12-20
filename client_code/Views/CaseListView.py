7d2f1adbcc5dbdce64daa75c030d564bf8fcba1e
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2023-12-20 11:17:22 -0500

    dmytro

--- client_code/Views/CaseListView.py
+++ client_code/Views/CaseListView.py
@@ -8,14 +8,9 @@ class CaseListView(GridView2):
     def __init__(self, **kwargs):
         print('CaseListView')
 
-        context_menu_items = [
-            {'id': 'select_tenant', 'label': 'Open Dashboard', 'action': self.open_dashboard},
-        ]
-
         super().__init__(
             model='Case',
             # view_config=view_config,
-            context_menu_items=context_menu_items,
             **kwargs)
 
 
