d7b2ead3660cfc666808b0f928f8545d6f933690
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2023-12-22 03:32:42 -0500

    dmytro

--- client_code/Views/ContactView.py
+++ client_code/Views/ContactView.py
@@ -4,13 +4,12 @@ from DevFusion.components.GridView2 import GridView2
 
 class ContactView(GridView2):
 	def __init__(self, **kwargs):
-		# print("ContactView")
-		# view_config = {
-		# 	'model': 'Contact',
-		# 	'columns': [
-		# 		{'name': 'first_name', 'label': 'Name'},
-		# 		{'name': 'last_name', 'label': 'Name'}
-		# 	]
-		# }
-		# super().__init__(model='Contact', view_config=view_config, *kwargs)
-		super().__init__(**kwargs)
+		print("ContactView")
+		view_config = {
+			'model': 'Contact',
+			'columns': [
+				{'name': 'first_name', 'label': 'Name'},
+				{'name': 'last_name', 'label': 'Name'}
+			]
+		}
+		super().__init__(model='Contact', view_config=view_config, *kwargs)
