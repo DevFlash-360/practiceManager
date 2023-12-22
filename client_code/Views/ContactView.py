b48c5d4f763df6362fb07e99b2b98cf97806b4d8
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2023-12-22 03:29:44 -0500

    dmytro

--- client_code/Views/ContactView.py
+++ client_code/Views/ContactView.py
@@ -1,16 +1,16 @@
 import anvil.server
 from DevFusion.components.GridView2 import GridView2
-from AnvilFusion.tools.utils import AppEnv
 
 
 class ContactView(GridView2):
 	def __init__(self, **kwargs):
-		print("ContactView")
-		view_config = {
-			'model': 'Contact',
-			'columns': [
-				{'name': 'first_name', 'label': 'Name'},
-				{'name': 'last_name', 'label': 'Name'}
-			]
-		}
-		super().__init__(model='Contact', view_config=view_config, *kwargs)
+		# print("ContactView")
+		# view_config = {
+		# 	'model': 'Contact',
+		# 	'columns': [
+		# 		{'name': 'first_name', 'label': 'Name'},
+		# 		{'name': 'last_name', 'label': 'Name'}
+		# 	]
+		# }
+		# super().__init__(model='Contact', view_config=view_config, *kwargs)
+		super().__init__(**kwargs)
