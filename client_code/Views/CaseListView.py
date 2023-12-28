8c8064cf6118e0b5c075602ef92f002e044eae64
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2023-12-28 09:37:20 -0500

    dmytro

--- client_code/Views/CaseListView.py
+++ client_code/Views/CaseListView.py
@@ -27,6 +27,8 @@ class CaseListView(GridView2):
         print(f"CaseListview/open_dashboard args = {args}")
         AppEnv.navigation.show_menu('case_menu', subcomponent='case_dashboard',
                                     props={'case_uid': args.rowData.uid})
+        jQuery('#pm-sidebar-menu li.e-level-1').removeClass('e-active')
+        jQuery('#pm-sidebar-menu li[data-uid="case_dashboard"]').addClass('e-active')
         jQuery('#pm-sidebar-menu li[data-uid="case_dashboard"] div.e-icon-wrapper div.e-icons').removeClass('e-icon-expandable')
         jQuery('#pm-sidebar-menu li[data-uid="case_dashboard"] div.e-icon-wrapper div.e-icons').addClass('e-icon-collapsible')
         jQuery('#pm-sidebar-menu li[data-uid="case_dashboard"] ul')[0].style.display = "block"
