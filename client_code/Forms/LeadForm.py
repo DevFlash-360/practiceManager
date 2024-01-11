68d4a59e5c9a9c4f5272f28540d7f9b540a6d9c3
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2024-01-11 15:20:54 -0500

    dmytro

--- client_code/Forms/LeadForm.py
+++ client_code/Forms/LeadForm.py
@@ -134,7 +134,7 @@ class LeadForm(FormBase):
     def form_open(self, args):
         super().form_open(args)
         print(f"form_open self.data = {self.data}")
-        if self.data is None or self.data == {}:
+        if self.case_status.value is None:
             self.case_status.value = "Open"    
 
     def after_open(self):
