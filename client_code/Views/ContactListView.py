eb308b52edd25c7aac8c2be9525ebe55ce95d55b
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2024-01-15 02:48:38 -0500

    dmytro

--- client_code/Views/ContactListView.py
+++ client_code/Views/ContactListView.py
@@ -2,13 +2,13 @@ import anvil.server
 from DevFusion.components.GridView2 import GridView2
 from AnvilFusion.tools.utils import AppEnv
 from anvil.js.window import ej, jQuery
-from ..app.models import Lead, User
+from ..app.models import Contact
 
 
 class ContactListView(GridView2):
     def __init__(self, **kwargs):
         view_config = {
-            'model': 'Contact',
+            'model': 'Contacts',
             'columns': [
                 # {'name': 'full_name', 'label': 'Name'},
                 {'name': 'contact_group', 'label': 'Group'},
@@ -18,4 +18,4 @@ class ContactListView(GridView2):
                 {'name': 'work_phone', 'label': 'Work Phone'},
             ]
         }
-        super().__init__(model='Contact', view_config=view_config, **kwargs)
+        super().__init__(model='Contacts', view_config=view_config, **kwargs)
