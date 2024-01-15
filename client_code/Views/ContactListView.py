20994f7aab83bdb6e6641c4ef643138500c7aced
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2024-01-15 02:58:02 -0500

    dmytro

--- client_code/Views/ContactListView.py
+++ client_code/Views/ContactListView.py
@@ -10,10 +10,10 @@ class ContactListView(GridView2):
         view_config = {
             'model': 'Contact',
             'columns': [
-                # {'name': 'full_name', 'label': 'Name'},
-                # {'name': 'contact_group', 'label': 'Group'},
-                # {'name': 'email', 'label': 'Email'},
-                # {'name': 'mobile_phone', 'label': 'Mobile Phone'},
+                {'name': 'full_name', 'label': 'Name'},
+                {'name': 'contact_group', 'label': 'Group'},
+                {'name': 'email', 'label': 'Email'},
+                {'name': 'mobile_phone', 'label': 'Mobile Phone'},
                 {'name': 'work_phone', 'label': 'Work Phone'},
             ]
         }
