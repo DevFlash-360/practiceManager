6fa13f8256d449c26657f2977d35778c381917a1
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2024-01-17 10:13:46 -0500

    dmytro

--- client_code/Forms/LeadLostForm.py
+++ client_code/Forms/LeadLostForm.py
@@ -16,7 +16,13 @@ class LeadLostForm:
         self.container_el.setAttribute('id', self.container_id)
         self.target_el.append(self.container_el)
 
-        self.form_content = '<div>Form Content</div>'
+        self.lost_reason = DropdownInput(name='lost_reason', label='Lost Reason', options=[
+            'No Money', 'No Case', 'Calling for Information', 'Marketing', 'Wrong Number',
+            'Out of State', 'Outside Practice Area', 'Hired Someone Else']
+        )
+
+
+        self.form_content = f'<div id="{self.lost_reason.container_id}"></div>'
         self.form = ej.popups.Dialog({
             'header': 'Lost Lead',
             'content': self.form_content,
@@ -48,6 +54,7 @@ class LeadLostForm:
 
     def form_save(self, args):
         print("form_save")
+
     
     def form_cancel(self, args):
         print("form_cancel")
