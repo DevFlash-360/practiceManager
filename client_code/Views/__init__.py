0bd04388f2501039c9aa616ab22f1c5ead56d2e6
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2024-01-23 01:43:54 -0500

    dmytro

--- client_code/Views/__init__.py
+++ client_code/Views/__init__.py
@@ -5,3 +5,4 @@ from .EventScheduleView import EventScheduleView
 from .TaskListView import TaskListView
 from .LeadListView import LeadListView
 from .ContactListView import ContactListView
+from .CaseAgendaView import CaseAgendaView
