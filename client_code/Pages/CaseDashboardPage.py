f1a7edddab59a408704292121d4a288033a6e759
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2024-01-30 00:34:51 -0500

    dmytro

--- client_code/Pages/CaseDashboardPage.py
+++ client_code/Pages/CaseDashboardPage.py
@@ -13,8 +13,10 @@ class CaseDashboardPage(DashboardPage):
         self.case_uid = kwargs.get('case_uid', None)
         if self.case_uid:
             set_cookie('case_uid', self.case_uid)
+            print(f"set_cookie {self.case_uid}")
         else:
             self.case_uid = get_cookie('case_uid')
+            print(f"get_cookie {self.case_uid}")
         print('CaseDashboardPage', self.case_uid)
         self.case = Case.get(self.case_uid) if self.case_uid else None
         self.panel_container_style = DASHBOARD_PANEL_CONTAINER_STYLE
