c9db88597928afb2c1ce1cb920ed034354f2f64a
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2024-03-20 12:17:02 -0400

    dmytro

--- client_code/Views/SettlementCalculatorView.py
+++ client_code/Views/SettlementCalculatorView.py
@@ -17,8 +17,11 @@ class SettlementCalculatorView:
         self.treatment_reduction_id = f"treatment_{uuid.uuid4()}"
         self.client_net_id = f"treatment_{uuid.uuid4()}"
         self.attorney_net_id = f"treatment_{uuid.uuid4()}"
+        self.total_fees_id = f"total_fees_{uuid.uuid4()}"
         self.table_fees_id = f"Fee_table_{uuid.uuid4()}"
-        self.table_treatment_id = f"Treatment_table_{uuid.uuid4()}"
+        self.table_treatment_id = f"total_treatment_{uuid.uuid4()}"
+        self.total_treatment_id = f"total_treatment_{uuid.uuid4()}"
+        self.reduced_treatment_id = f"reduced_treatment_{uuid.uuid4()}"
     
         cases_data = Case.search()
         cases_data_for_dropdown = [{'id': case['uid'], 'text': case['case_name']} for case in cases_data]
@@ -31,6 +34,9 @@ class SettlementCalculatorView:
         self.treatment_reduction = ej.dropdowns.DropDownList({'dataSource': percentage_list})
         self.client_net = ej.inputs.TextBox({'floatLabelType': 'Auto'})
         self.attorney_net = ej.inputs.TextBox({'floatLabelType': 'Auto'})
+        self.total_fees = ej.inputs.TextBox({'floatLabelType': 'Auto'})
+        self.total_treatment = ej.inputs.TextBox({'floatLabelType': 'Auto'})
+        self.reduced_treatment = ej.inputs.TextBox({'floatLabelType': 'Auto'})
         
     def form_show(self):
         self.container_el.innerHTML = f'\
@@ -58,32 +64,65 @@ class SettlementCalculatorView:
                 <div class="col-md-3">\
                     <label>Client Net</label>\
                     <div class="form-group input-group">\
-                        <input id="{self.client_net_id}" type="number" class="form-control">\
+                        <input readonly id="{self.client_net_id}" type="number" class="form-control">\
                         <span class="input-group-addon"><i class="fa-light fa-dollar-sign"></i></span>\
                     </div>\
                     <label>Attorney/Firm Net</label>\
                     <div class="form-group input-group">\
-                        <input id="{self.attorney_net_id}" type="number" class="form-control">\
+                        <input readonly id="{self.attorney_net_id}" type="number" class="form-control">\
                         <span class="input-group-addon"><i class="fa-light fa-dollar-sign"></i></span>\
                     </div>\
                 </div>\
             </div>\
-            <table id="{self.table_fees_id}"></table>'
+            <table id="{self.table_fees_id}"></table>\
+            <div class="row">\
+                <div class="col-md-3">\
+                    <label>Total Fees & Costs</label>\
+                    <div class="form-group input-group">\
+                        <input readonly id="{self.total_fees_id}" type="number" class="form-control">\
+                        <span class="input-group-addon"><i class="fa-light fa-dollar-sign"></i></span>\
+                    </div>\
+                </div>\
+            </div>\
+            <table id="{self.table_treatment_id}"></table>\
+            <div class="row">\
+                <div class="col-md-3">\
+                    <label>Total Medical Treatment</label>\
+                    <div class="form-group input-group">\
+                        <input readonly id="{self.total_treatment_id}" type="number" class="form-control">\
+                        <span class="input-group-addon"><i class="fa-light fa-dollar-sign"></i></span>\
+                    </div>\
+                </div>\
+                <div class="col-md-3">\
+                    <label>Reduced Medical Treatment</label>\
+                    <div class="form-group input-group">\
+                        <input readonly id="{self.reduced_treatment_id}" type="number" class="form-control">\
+                        <span class="input-group-addon"><i class="fa-light fa-dollar-sign"></i></span>\
+                    </div>\
+                </div>\
+            </div>\
+            '
         
         self.dropdown_cases.appendTo(jQuery(f"#{self.dropdown_cases_id}")[0])
         self.contingency_fee.appendTo(jQuery(f"#{self.contingency_fee_id}")[0])
         self.settlement_offer.appendTo(jQuery(f"#{self.settlement_offer_id}")[0])
         self.treatment_reduction.appendTo(jQuery(f"#{self.treatment_reduction_id}")[0])
+        self.total_fees.appendTo(jQuery(f"#{self.total_fees_id}")[0])
         self.client_net.appendTo(jQuery(f"#{self.client_net_id}")[0])
         self.attorney_net.appendTo(jQuery(f"#{self.attorney_net_id}")[0])
+        self.total_treatment.appendTo(jQuery(f"#{self.total_treatment_id}")[0])
+        self.reduced_treatment.appendTo(jQuery(f"#{self.reduced_treatment_id}")[0])
     
     def destroy(self):
         self.dropdown_cases.destroy()
         self.contingency_fee.destroy()
         self.settlement_offer.destroy()
         self.treatment_reduction.destroy()
+        self.total_fees.destroy()
         self.client_net.destroy()
         self.attorney_net.destroy()
+        self.total_treatment.destroy()
+        self.reduced_treatment.destroy()
 
         if self.container_el:
             self.container_el.innerHTML = ''
