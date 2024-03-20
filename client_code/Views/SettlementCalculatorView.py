e1148f18b8616aeac7ede341dccda6c23072f939
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2024-03-20 02:49:27 -0400

    dmytro

--- client_code/Views/SettlementCalculatorView.py
+++ client_code/Views/SettlementCalculatorView.py
@@ -15,15 +15,20 @@ class SettlementCalculatorView:
     
         cases_data = Case.search()
         cases_data_for_dropdown = [{'id': case['uid'], 'text': case['case_name']} for case in cases_data]
-        self.dropdown_cases = ej.dropdowns.DropDownList({'dataSource': cases_data_for_dropdown})
+        self.dropdown_cases = ej.dropdowns.DropDownList({
+            'dataSource': cases_data_for_dropdown,
+            'placeholder': 'Select a Case'
+        })
         
     def form_show(self):
         self.container_el.innerHTML = f'\
             <div class="row">\
                 <div class="col-md-3">\
-                    <input type="text" id="{self.dropdown_cases_id}" />\
-                    <div class="input-group">\
-                        <input type="number" class="form-control">\
+                    <div class="form-group">\
+                        <input type="text" id="{self.dropdown_cases_id}" />\
+                    </div>\
+                    <div class="form-group input-group">\
+                        <input placeholder="Contingency Fee" type="number" class="form-control">\
                         <span class="input-group-addon"><i class="fa-light fa-percent"></i></span>\
                     </div>\
                 </div>\
