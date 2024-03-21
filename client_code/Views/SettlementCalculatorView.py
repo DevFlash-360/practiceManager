6a22805008d3b34716075e5b9ad242423c139d29
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2024-03-21 05:19:38 -0400

    dmytro

--- client_code/Views/SettlementCalculatorView.py
+++ client_code/Views/SettlementCalculatorView.py
@@ -28,7 +28,7 @@ class SettlementCalculatorView:
         cases_data = Case.search()
         cases_data_for_dropdown = [{'id': case['uid'], 'text': case['case_name']} for case in cases_data]
         self.dropdown_cases = ej.dropdowns.DropDownTree({
-            'dataSource': cases_data_for_dropdown
+            'fields': {'dataSource': cases_data_for_dropdown, 'value': 'id', 'text': 'text'}
         })
         self.contingency_fee = ej.inputs.TextBox({'floatLabelType': 'Auto'})
         self.settlement_offer = ej.inputs.TextBox({'floatLabelType': 'Auto'})
