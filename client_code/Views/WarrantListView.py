63c2313865a0428de6a02b321ac3f2b1c62bd3db
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2024-03-26 10:10:17 -0400

    dmytro

--- client_code/Views/WarrantListView.py
+++ client_code/Views/WarrantListView.py
@@ -10,10 +10,13 @@ from ..app.models import CaseStage, CaseStatus, Case
 class WarrantListView(GridView2):
     def __init__(self, **kwargs):
         pre_charge_uids = []
+        print("===== 1 ===== ")
         case_stage_pre_charge = CaseStage.search(name='Pre-Charge')
+        print("===== 2 ===== ")
         if case_stage_pre_charge:
             for ele in case_stage_pre_charge:
                 pre_charge_uids.append(ele['uid'])
+        print("===== 3 ===== ")
         view_config = {
             'model': 'Case',
             'columns': [
