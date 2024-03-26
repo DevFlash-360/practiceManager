85e437e356b803ff1418f8418772e072695046c6
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2024-03-26 03:53:02 -0400

    dmytro

--- client_code/Views/WarrantListView.py
+++ client_code/Views/WarrantListView.py
@@ -17,7 +17,7 @@ class WarrantListView(GridView2):
                 {'name': 'contacts.full_name', 'label': 'Contacts'},
             ]
         }
-        filters = {'case_stage.name': 'Pre-Charge', 'case_status.name': 'Open'}
+        filters = {'case_stage': {'name': 'Pre-Charge'}, 'case_status': {'name': 'Open'}}
         super().__init__(model='Case', view_config=view_config, filters=filters, **kwargs)
 
     def form_show(self, get_data=True, **args):
