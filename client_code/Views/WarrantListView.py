99da5743ea54ff52796a515b6679fbe274c7339c
Dmytro.S <dmytro.savchenko.solutions@gmail.com>
2024-03-26 11:48:21 -0400

    dmytro

--- client_code/Views/WarrantListView.py
+++ client_code/Views/WarrantListView.py
@@ -30,7 +30,7 @@ class WarrantListView(GridView2):
         # else:
         #     filters = None
         filters = {'case_stage': {'name': 'Pre-Charge'}, 'case_status': {'name': 'Open'}}
-        super().__init__(model='Case', view_config=view_config, **kwargs)
+        super().__init__(model='Case', view_config=view_config, filters=filters, **kwargs)
 
     def form_show(self, get_data=True, **args):
         print("WarrantSearch/form_show")
